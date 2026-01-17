22cc64ba979404d64600986be0428042eb007159
SharzyL <me@sharzy.in>
2026-01-17 23:32:19 +0800

    hm_configs.rootbase: container specific config

--- hm_configs/rootbase.nix
+++ hm_configs/rootbase.nix
@@ -4,4 +4,9 @@
   home.username = "root";
   home.homeDirectory = "/root";
   nivium.profile = "full";
+
+  # container-specific config
+  # may requires DBUS_SESSION_BUS_ADDRESS=/dev/null
+  targets.genericLinux.enable = true;
+  systemd.user.startServices = false;
 }
